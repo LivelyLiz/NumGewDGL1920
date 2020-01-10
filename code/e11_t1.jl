43342cff04d228e2fa52a6861844a4c91c103e83
Lisa <lisa.piotrowski@st.ovgu.de>
2020-01-10 16:33:16 +0100

    Add Exercise 11

---
+++ code/e11_t1.jl
@@ -0,0 +1,50 @@
+using Plots
+gr()
+
+#Task 1
+
+# a)
+function R_a(z, s)
+    res = 0
+    for k = 0:s
+        res += z^k/factorial(k)
+    end
+    return res
+end
+
+subplots_a = []
+
+for i in 1:5
+    subplot = contour(collect(-5:0.1:5), collect(-5:0.1:5), (x, y) -> abs(R_a(x + y*1im, i)), levels=[1], title = "ERK Order $i", xlabel = "Re", ylabel = "Im")
+    push!(subplots_a, subplot)
+end
+
+plot_a = plot(subplots_a..., size=(1024, 512))
+
+savefig(plot_a, "../plot/e11_t1_a.pdf")
+
+# b)
+R_b_1(z) = 1/12.0*z^4.0 + 1/6*z^3.0 + 1/2*z^2.0 + z + 1
+R_b_2(z) = 3/18*z^3.0 + 1/2*z^2.0 + z + 1
+
+plot_b = plot(contour(-5:0.1:5, -5:0.1:5, (x, y) -> abs(R_b_1(x + y*1im)), levels=[1], title = "1", xlabel = "Re", ylabel = "Im"),
+              contour(-5:0.1:5, -5:0.1:5, (x, y) -> abs(R_b_2(x + y*1im)), levels=[1], title = "2", xlabel = "Re", ylabel = "Im"),
+              size=(512, 200))
+
+savefig(plot_b, "../plot/e11_t1_b.pdf")
+
+# d)
+R_c_implRK(z) = 1/(1-z)
+R_c_implMid(z) = (z+2)/(2-z)
+R_c_SDIRK(z, gamma) = 1 + z/((1-gamma*z)^2.0) * (1-2*gamma*z+1/2*z)
+
+gamma1 = (3 + sqrt(3))/6
+gamma2 = (3 - sqrt(3))/6
+
+plot_c = plot(contour(-5:0.1:5, -5:0.1:5, (x, y) -> abs(R_c_implRK(x + y*1im)), levels=[1], title = "Impl. Euler", xlabel = "Re", ylabel = "Im"),
+              contour(-5:0.1:5, -5:0.1:5, (x, y) -> abs(R_c_implMid(x + y*1im)), levels=[1], title = "Impl. Midpoint", xlabel = "Re", ylabel = "Im"),
+              contour(-1:0.1:12, -7:0.1:7, (x, y) -> abs(R_c_SDIRK(x + y*1im, gamma1)), levels=[1], title = "SDIRK 3, +", xlabel = "Re", ylabel = "Im"),
+              contour(-15:0.1:1, -8:0.1:8, (x, y) -> abs(R_c_SDIRK(x + y*1im, gamma2)), levels=[1], title = "SDIRK 3, -", xlabel = "Re", ylabel = "Im"),
+              size=(1024, 1024))
+
+savefig(plot_c, "../plot/e11_t1_c.pdf")
